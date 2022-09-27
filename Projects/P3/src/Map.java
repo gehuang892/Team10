4bd3c2e161b3fd7490bd4eb1006437b4995879d3
Kishore Nagarajan <kishorenaga4@gmail.com>
2022-09-27 19:32:08 -0400

    map-eatCookie

--- Projects/P3/src/Map.java
+++ Projects/P3/src/Map.java
@@ -70,6 +70,22 @@ public class Map {
   public JComponent eatCookie(String name) {
     // update locations, components, field, and cookies
     // the id for a cookie at (10, 1) is tok_x10_y1
-    return null;
+
+    // Update location, remove given cookie id
+    Location l = locations.remove(name);
+
+    if (l == null) {
+      return null;
+    }
+    
+    // Use removed location to update the field var.
+    field.remove(l);
+
+    // Update cookies var
+    cookies++;
+
+    // Update components var
+    return components.remove(name);
+    
   }
 }
