9c1c3e87c963b16be2cff1774d6026fd995aff34
richardmbui <rbui@terpmail.umd.edu>
2022-09-27 11:53:33 -0400

    Added is_pacman_in_range() for Ghost and testing

--- Projects/P3/tests/TestPacManValidMoves.java
+++ Projects/P3/tests/TestPacManValidMoves.java
@@ -1,9 +1,25 @@
 import java.io.*;
+import java.util.ArrayList;
+
 import junit.framework.*;
 
 public class TestPacManValidMoves extends TestCase {
 
   public void testPacManValidMoves() throws FileNotFoundException {
-    return null;
+    MainFrame frame = new MainFrame();
+    PacMan pacMan = frame.addPacMan(new Location(1, 1));
+
+    ArrayList<Location> result = pacMan.get_valid_moves();
+    if (result.size() != 2) {
+      fail("Expect 2 possible moves from this location");
+    }
+
+
+    PacMan pacMan2 = frame.addPacMan(new Location(1, 3));
+
+    result = pacMan2.get_valid_moves();
+    if (result.size() != 3) {
+      fail("Expect 2 possible moves from this location");
+    }
   }
 }
