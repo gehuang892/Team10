884f95a7a2d6fd515aa55a8274b2ce1a660b6f55
Adrian Paras <aparas@terpmail.umd.edu>
2022-09-28 09:47:30 -0400

    Update move

--- Projects/P3/src/PacMan.java
+++ Projects/P3/src/PacMan.java
@@ -2,30 +2,36 @@ import java.util.ArrayList;
 import javax.swing.JComponent;
 
 public class PacMan {
-  String myName;
-  Location myLoc;
-  Map myMap;
-  Location shift;
+	String myName;
+	Location myLoc;
+	Map myMap;
+	Location shift;
 
-  public PacMan(String name, Location loc, Map map) {
-    this.myLoc = loc;
-    this.myName = name;
-    this.myMap = map;
-  }
+	public PacMan(String name, Location loc, Map map) {
+		this.myLoc = loc;
+		this.myName = name;
+		this.myMap = map;
+	}
 
-  public ArrayList<Location> get_valid_moves() {
-    return null;
-  }
+	public ArrayList<Location> get_valid_moves() {
+		return null;
+	}
 
-  public boolean move() {
-    return false;
-  }
+	public boolean move() {
+		ArrayList<Location> moves = get_valid_moves();
+		if (!moves.isEmpty()) {
+			myLoc.x = moves.get(0).x;
+			myLoc.y = moves.get(0).y;
+			return true;
+		}
+		return false;
+	}
 
-  public boolean is_ghost_in_range() {
-    return false;
-  }
+	public boolean is_ghost_in_range() {
+		return false;
+	}
 
-  public JComponent consume() {
-    return null;
-  }
+	public JComponent consume() {
+		return null;
+	}
 }
