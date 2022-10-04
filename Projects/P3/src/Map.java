046871d568dab6ac1a8c2b1f68e56b5e6d4467a4
Tina Le <tinale218@gmail.com>
2022-10-04 15:32:10 -0400

    Fixed map-attack

--- Projects/P3/src/Map.java
+++ Projects/P3/src/Map.java
@@ -109,7 +109,6 @@ public class Map {
     return false;
   }
 
-
   public JComponent eatCookie(String name) {
     // update locations, components, field, and cookies
     // the id for a cookie at (10, 1) is tok_x10_y1
