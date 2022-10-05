d19227d7f2958f613e84cd2964b8ad8e279eb6c6
richardmbui <64283065+richardmbui@users.noreply.github.com>
2022-10-05 12:16:44 -0400

    Update TestGhostMove.java
    removed ghostmovetest in map branch

--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -3,13 +3,5 @@ import junit.framework.*;
 
 public class TestGhostMove extends TestCase {
 
-  public void testGhostMove() throws FileNotFoundException {
-	  
-	  Map m = new Map(30);
-	  m.add("ghost", new Location(9, 11), new GhostComponent (9, 11, 2), Map.Type.GHOST);
-	  Ghost g = new Ghost("ghost", new Location(9, 11), m);
-	  assertTrue(g.move() == false);
-	  m.add("cookie", new Location(9, 12), new CookieComponent (9, 12, 2), Map.Type.COOKIE);
-	  assertTrue(g.move() == true);
-  }
+  public void testGhostMove() throws FileNotFoundException {}
 }
