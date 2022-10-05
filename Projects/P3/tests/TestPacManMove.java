c1c80425bd05018220c4c6733873dff32a2b8bfe
Adrian Paras <aparas@terpmail.umd.edu>
2022-10-04 22:38:14 -0400

    Update pacman move

--- Projects/P3/tests/TestPacManMove.java
+++ Projects/P3/tests/TestPacManMove.java
@@ -4,6 +4,11 @@ import junit.framework.*;
 public class TestPacManMove extends TestCase {
 
   public void testPacManMove() throws FileNotFoundException {
-    return null;
+	  Map m = new Map(30);
+	  m.add("pacman", new Location(9, 11), new PacManComponent (9, 11, 2), Map.Type.PACMAN);
+	  PacMan p = new PacMan("pacman", new Location(9, 11), m);
+	  assertTrue(p.move() == false);
+	  m.add("cookie", new Location(9, 12), new CookieComponent (9, 12, 2), Map.Type.COOKIE);
+	  assertTrue(p.move() == true);
   }
 }
