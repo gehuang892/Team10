fb97b3f1ae078bd1c43494bab828a426ffdb9486
Tina Le <tinale218@gmail.com>
2022-10-05 04:26:20 -0400

    Added pacman-consume test

--- Projects/P3/tests/TestConsume.java
+++ Projects/P3/tests/TestConsume.java
@@ -4,6 +4,23 @@ import junit.framework.*;
 public class TestConsume extends TestCase {
 
   public void testConsume() throws FileNotFoundException {
-    return null;
+    MainFrame frame = new MainFrame();
+    Map m = frame.getMap();
+
+    Map m = new Map(30);
+	  m.add("pacman", new Location(9, 12), new PacManComponent (9, 12, 20), Map.Type.PACMAN);
+	  PacMan pacman = new PacMan("pacman", new Location(9, 12), m);
+    
+    // No cookie exists
+    assertTrue(pacman.consume() == null);
+    
+    // Cookie exists but is not in pacman's current coordinates
+	  m.add("cookie", new Location(9, 10), new CookieComponent (9, 10, 20), Map.Type.COOKIE);
+    assertTrue(pacman.consume() == null);
+
+    // Cookie exists and is in pacman's current coordinates
+    m.add("cookie", new Location(9, 12), new CookieComponent (9, 12, 20), Map.Type.COOKIE);
+    assertTrue(m.getLoc().contains(Map.Type.COOKIE));
+    assertTrue(pacman.consume() != null && pacman.consume() == CookieComponent(9, 12, 20));
   }
 }
