853fc44a880cfec504fbe680b155d23c90c83e22
Kishore Nagarajan <kishorenaga4@gmail.com>
2022-10-16 12:55:38 -0400

    Fixing tests and adding junit file

--- Projects/P3/tests/TestMapEatCookie.java
+++ Projects/P3/tests/TestMapEatCookie.java
@@ -4,12 +4,14 @@ import junit.framework.*;
 public class TestMapEatCookie extends TestCase {
 
   public void testMapEatCookie() throws FileNotFoundException {
-    MainFrame frame = new MainFrame();
+    NoFrame frame = new NoFrame();
     Map m = frame.getMap();
 
+    // Try to eat an unknown cookie, expect null since no Cookie should exist
     assertTrue(m.eatCookie("test") == null);
 
+    // a Cookie already exists at Location (1,1), so placing Pacman at this location should return a cookie
     PacMan pacman = frame.addPacMan(new Location(1,1));
-     assertTrue(m.eatCookie("pacman") != null);
+    assertTrue(m.eatCookie("pacman") != null);
   }
 }
