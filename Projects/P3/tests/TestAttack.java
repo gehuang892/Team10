205bdbb355430c54aba539ee0e29101d4c48d6f6
Tina Le <tinale218@gmail.com>
2022-10-17 18:29:09 -0400

    Fixed tests

--- Projects/P3/tests/TestAttack.java
+++ Projects/P3/tests/TestAttack.java
@@ -5,24 +5,25 @@ import junit.framework.*;
 public class TestAttack extends TestCase {
 
   public void testAttack() throws FileNotFoundException {
-    MainFrame frame = new MainFrame();
+    NoFrame frame = new NoFrame();
     Map map = frame.getMap();
 
-    Ghost ghost1 = frame.addGhost(new Location(9, 11), "name", Color.red);
+    Ghost ghost1 = frame.addGhost(new Location(6, 11), "name", Color.red);
 
     // No pacman exists
     assertTrue(ghost1.attack() == false);
 
     PacMan pacman = frame.addPacMan(new Location(1, 1));
 
-    // Pacman out of range 
+    // Pacman added but is out of range 
     assertTrue(ghost1.attack() == false);
     assertTrue(ghost1.attack() == map.attack("name"));
 
+    Ghost ghost2 = frame.addGhost(new Location(9, 11), "name", Color.red);
     PacMan pacman2 = frame.addPacMan(new Location(9, 12));
     
-    // Pacman exists and is within range
-    assertTrue(ghost1.attack() == true);
-    assertTrue(ghost1.attack() == map.attack("name"));
+    // Pacman exists and is within range of newly added ghost2
+    assertTrue(ghost2.attack() == true);
+    assertTrue(ghost2.attack() == map.attack("name"));
   }
 }
