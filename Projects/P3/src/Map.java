5b9e1f9c5def525edc725201c28304dd409a0b9a
richardmbui <rbui@terpmail.umd.edu>
2022-10-18 12:24:22 -0400

    Fixed tests

--- Projects/P3/src/Map.java
+++ Projects/P3/src/Map.java
@@ -54,7 +54,7 @@ public class Map {
     if ((type == Map.Type.PACMAN) || (type == Map.Type.GHOST)) {
       int x = loc.x;
       int y = loc.y;
-      if (getLoc(loc) != null && !getLoc(loc).contains(Map.Type.WALL)) {
+      if (!getLoc(loc).contains(Map.Type.WALL)) {
         components.get(name).setLocation(x, y);
         locations.put(name, loc);
         if (!field.containsKey(loc)) field.put(loc, new HashSet<Type>());
