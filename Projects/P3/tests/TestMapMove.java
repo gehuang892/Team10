f2fa49f0ace2a8ec0f94b569296cf1e90f7d0b2a
richardmbui <rbui@terpmail.umd.edu>
2022-10-18 12:13:22 -0400

    Fixed tests

--- Projects/P3/tests/TestMapMove.java
+++ Projects/P3/tests/TestMapMove.java
@@ -4,11 +4,15 @@ import junit.framework.*;
 public class TestMapMove extends TestCase {
 
   public void testMapMove() throws FileNotFoundException {
-    MainFrame frame = new MainFrame();
+    NoFrame frame = new NoFrame();
     Map m = frame.getMap();
+    Location loc = new Location(9,11);
+    m.add("pacman", loc, new PacManComponent (9, 11, 2), Map.Type.PACMAN);
+    m.add("wall", new Location(8, 12), new WallComponent (9, 11, 2), Map.Type.WALL);
 
-    Location loc = new Location(1,1);
-    assertTrue(m.move("pacman", loc, Map.Type.PACMAN));
-    assertFalse(m.move("wall", loc, Map.Type.PACMAN));
+    Location dest = new Location(9, 12);
+    
+    assertTrue(m.move("pacman", dest, Map.Type.PACMAN));
+    assertFalse(m.move("wall", dest, Map.Type.WALL));
   }
 }
