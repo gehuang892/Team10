efa436482d58ed7973d8617f912c6c3f8350d8fa
richardmbui <rbui@terpmail.umd.edu>
2022-10-18 12:15:12 -0400

    Fixed tests

--- Projects/P3/tests/TestMapMove.java
+++ Projects/P3/tests/TestMapMove.java
@@ -8,11 +8,8 @@ public class TestMapMove extends TestCase {
     Map m = frame.getMap();
     Location loc = new Location(9,11);
     m.add("pacman", loc, new PacManComponent (9, 11, 2), Map.Type.PACMAN);
-    m.add("wall", new Location(8, 12), new WallComponent (9, 11, 2), Map.Type.WALL);
-
     Location dest = new Location(9, 12);
     
     assertTrue(m.move("pacman", dest, Map.Type.PACMAN));
-    assertFalse(m.move("wall", dest, Map.Type.WALL));
   }
 }
